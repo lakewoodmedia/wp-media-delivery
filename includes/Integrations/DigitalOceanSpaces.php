f414cade92c0585edab4079452cbb56b49de18c9
adam-fuunction <adam@fuunction.agency>
2025-05-22 19:10:47 +0800

    Update README to include installation instructions and usage examples

--- includes/Integrations/DigitalOceanSpaces.php
+++ includes/Integrations/DigitalOceanSpaces.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace Advanced_Media_Offloader\Integrations;
+namespace WP_Media_Delivery\Integrations;
 
-use Advanced_Media_Offloader\Abstracts\S3_Provider;
+use WP_Media_Delivery\Abstracts\S3_Provider;
 use WPFitter\Aws\S3\S3Client;
 
 class DigitalOceanSpaces extends S3_Provider
@@ -23,33 +23,33 @@ class DigitalOceanSpaces extends S3_Provider
 	{
 		return new S3Client([
 			'version' => 'latest',
-			'endpoint' => defined("ADVMO_DOS_ENDPOINT") ? ADVMO_DOS_ENDPOINT : '',
-			'region' => defined("ADVMO_DOS_REGION") ? ADVMO_DOS_REGION : 'us-east-1',
+			'endpoint' => defined("WPMD_DOS_ENDPOINT") ? WPMD_DOS_ENDPOINT : '',
+			'region' => defined("WPMD_DOS_REGION") ? WPMD_DOS_REGION : 'us-east-1',
 			'credentials' => [
-				'key' => defined("ADVMO_DOS_KEY") ? ADVMO_DOS_KEY : '',
-				'secret' => defined("ADVMO_DOS_SECRET") ? ADVMO_DOS_SECRET : '',
+				'key' => defined("WPMD_DOS_KEY") ? WPMD_DOS_KEY : '',
+				'secret' => defined("WPMD_DOS_SECRET") ? WPMD_DOS_SECRET : '',
 			]
 		]);
 	}
 
 	public function getBucket()
 	{
-		return defined("ADVMO_DOS_BUCKET") ? ADVMO_DOS_BUCKET : null;
+		return defined("WPMD_DOS_BUCKET") ? WPMD_DOS_BUCKET : null;
 	}
 
 	public function getDomain()
 	{
-		return defined('ADVMO_DOS_DOMAIN') ? trailingslashit(ADVMO_DOS_DOMAIN) : '';
+		return defined('WPMD_DOS_DOMAIN') ? trailingslashit(WPMD_DOS_DOMAIN) : '';
 	}
 
 	public function credentialsField()
 	{
 		$requiredConstants = [
-			'ADVMO_DOS_KEY' => 'Your DigitalOcean Spaces Access Key',
-			'ADVMO_DOS_SECRET' => 'Your DigitalOcean Spaces Secret Key',
-			'ADVMO_DOS_ENDPOINT' => 'Your DigitalOcean Spaces Endpoint URL',
-			'ADVMO_DOS_BUCKET' => 'Your DigitalOcean Spaces Bucket Name',
-			'ADVMO_DOS_DOMAIN' => 'Your Custom Domain',
+			'WPMD_DOS_KEY' => 'Your DigitalOcean Spaces Access Key',
+			'WPMD_DOS_SECRET' => 'Your DigitalOcean Spaces Secret Key',
+			'WPMD_DOS_ENDPOINT' => 'Your DigitalOcean Spaces Endpoint URL',
+			'WPMD_DOS_BUCKET' => 'Your DigitalOcean Spaces Bucket Name',
+			'WPMD_DOS_DOMAIN' => 'Your Custom Domain',
 		];
 
 		echo $this->getCredentialsFieldHTML($requiredConstants);
